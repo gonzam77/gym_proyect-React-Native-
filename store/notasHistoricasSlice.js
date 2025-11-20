0f5e9652de8f998c7d3220b128ae45c71fec5b30
gonzam77 <gonzam77@gmail.com>
2025-11-20 13:08:41 -0300

    feat(notasHistoricasSlice): Agregar un estado inicial de ejemplo

--- store/notasHistoricasSlice.js
+++ store/notasHistoricasSlice.js
@@ -1,7 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  notasHistoricas: [],
+  notasHistoricas: [
+    {
+      id:1,
+      titulo:'Registro de comidas',
+      notas:[{
+        id:1,
+        fecha:1732109085123,
+        nota:'Pollo al horno con ensalada de lechugas'
+      }]
+
+    },
+    {
+      id:2,
+      titulo:'Logros Diarios',
+      notas:[{
+        id:1,
+        fecha:1732109085123,
+        nota:'Hoy hice mi primera dominada.'
+      }]
+    },
+    {
+      id:3,
+      titulo:'Comentarios Diarios',
+      notas:[{
+        id:1,
+        fecha:1732109085123,
+        nota:'Hoy me senti con menos fuerza que ayer.'
+      }]
+    }  
+  ],
 };
 
 const notasHistoricasSlice = createSlice({
