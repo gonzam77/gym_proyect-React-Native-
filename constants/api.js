96e421cae864beebc49d182433adfc13580b9205
gonzam77 <gonzam77@gmial.com>
2026-06-17 00:30:20 -0300

    chore:cambio de direccion de la api

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://2.25.189.180:5000';
+export const API_BASE_URL = 'https://api-rutina360.devgm.com.ar';
+// export const API_BASE_URL = 'http://2.25.189.180:5000';
 // export const API_BASE_URL = 'https://rutina360-server.onrender.com';
 
 export const API_ENDPOINTS = {
